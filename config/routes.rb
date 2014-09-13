b8a3353704e5883dfb7cab01d44542ce0c70f417
Nataly <terskaya.nv@gmail.com>
2014-09-13 15:38:50 +0400

    HEROKU ADDED

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root to: 'store#index', as: 'store'
   # root 'welcome#index'
 
   # Example of regular route:
